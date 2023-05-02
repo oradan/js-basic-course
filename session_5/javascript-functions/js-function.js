8f5dc78a2677f26c8f49a0b9ae7729ede200a31e
oradan <oradan@pentalog.com>
2023-05-02 11:13:31 +0300

    Add files for session nr 5

---
+++ session_5/javascript-functions/js-function.js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+// https://builtin.com/software-engineering-perspectives/javascript-call-stack
+// https://javascript.info/recursion  execution context
+
+
+//Function declarations
+
+function myFunction() {
+
+}
+
+//Function expressions
+
+//Anonymous functions
+
+//Function hoisting
+//Function scope
+//Nested functions and closures
+
+
+// set default values to the parameters of function
+
