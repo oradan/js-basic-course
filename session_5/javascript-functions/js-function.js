307bf526f0b2ae43761e8d496e76cc9679d80784
oradan <oradan@pentalog.com>
2023-05-02 15:05:39 +0300

    Add content for session nr 5

--- session_5/javascript-functions/js-function.js
+++ session_5/javascript-functions/js-function.js
@@ -1,22 +1,22 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
-// https://builtin.com/software-engineering-perspectives/javascript-call-stack
-// https://javascript.info/recursion  execution context
+// https://www.jsv9000.app/
+// http://latentflip.com/loupe/?code=Cgo%3D!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 
 
 //Function declarations
 
-function myFunction() {
-
-}
 
 //Function expressions
-
 //Anonymous functions
-
+//Function scope
+//Function parameters
+//set default values to the parameters of function
+//return from function
 //Function hoisting
 //Function scope
 //Nested functions and closures
+//setTimeout()
+//setInterval()
 
 
-// set default values to the parameters of function
 
