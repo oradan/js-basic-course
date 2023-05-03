f42f4b8184cdee4ae33a33d81c44eda5abce8e80
lavinia-firezar <lavinia.firezar@gmail.com>
2023-05-03 04:58:28 +0300

    resolved ecercises

--- session_1/javascript-operators/js-operators.js
+++ session_1/javascript-operators/js-operators.js
@@ -6,8 +6,22 @@
 
 // ternary operator
 
-
 //exercises
-
 // given const prices = [33, 45, 66, 7, 8]; calculate the total price , use a for loop;
+function calcSumPrices(prices) {
+  let totalPrice = 0;
+  for (let i = 0; i < prices.length; i++) {
+    totalPrice += prices[i];
+  }
+  return totalPrice;
+}
+console.log(calcSumPrices([33, 45, 66, 7, 8]));
+
 // given const prices = [33, 45, 66, 7, 8]; given a discount of 10% return a new array with the new discounted prices (EX arr = [10] disc 10% new arr=[9])
+const prices = [33, 45, 66, 7, 8];
+let discountedPrices = [];
+for (let i = 0; i < prices.length; i++) {
+  let newPrice = Math.round(prices[i] - (10 / 100) * prices[i]);
+  discountedPrices.push(newPrice);
+}
+console.log(discountedPrices);
--- session_3/javascript-exercisess/js-array-obj-ex.js
+++ session_3/javascript-exercisess/js-array-obj-ex.js
@@ -1,99 +1,146 @@
 const peeps = [
-    {
-        name: {
-            first: 'Jerod',
-            last: 'Smith'
-        },
-        age: 25,
-        id: 1,
+  {
+    name: {
+      first: "Jerod",
+      last: "Smith",
     },
-    {
-        name: {
-            first: 'Jen',
-            last: 'Jones'
-        },
-        age: 30,
-        id: 2,
+    age: 25,
+    id: 1,
+  },
+  {
+    name: {
+      first: "Jen",
+      last: "Jones",
     },
-    {
-        name: {
-            first: 'Terrance',
-            last: 'Adams'
-        },
-        age: 26,
-        id: 3,
-    }
+    age: 30,
+    id: 2,
+  },
+  {
+    name: {
+      first: "Terrance",
+      last: "Adams",
+    },
+    age: 26,
+    id: 3,
+  },
 ];
 
 const comments = {
-    293: {
-        text: 'I love pizza!',
-        userId: 1,
-        rating: 4,
-    },
-    123: {
-        text: 'I love pizza more than everyone!',
-        userId: 1,
-        rating: 5,
-    },
-    4223: {
-        text: 'Burgers are best.',
-        userId: 3,
-        rating: 4,
-    },
-    4267: {
-        text: 'Bears eat beets.',
-        userId: 3,
-        rating: 3,
-    },
-    1561: {
-        text: 'Best service ever!',
-        userId: 2,
-        rating: 5,
-    },
-    1899: {
-        text: 'This was gross!',
-        userId: 3,
-        rating: 1,
-    },
-    415: {
-        text: 'Much better than last time!',
-        userId: 3,
-    }
+  293: {
+    text: "I love pizza!",
+    userId: 1,
+    rating: 4,
+  },
+  123: {
+    text: "I love pizza more than everyone!",
+    userId: 1,
+    rating: 5,
+  },
+  4223: {
+    text: "Burgers are best.",
+    userId: 3,
+    rating: 4,
+  },
+  4267: {
+    text: "Bears eat beets.",
+    userId: 3,
+    rating: 3,
+  },
+  1561: {
+    text: "Best service ever!",
+    userId: 2,
+    rating: 5,
+  },
+  1899: {
+    text: "This was gross!",
+    userId: 3,
+    rating: 1,
+  },
+  415: {
+    text: "Much better than last time!",
+    userId: 3,
+  },
 };
-
 
 // EASIER CHALLENGES (using the peeps array) //
 
 // 1. Count the number of people in the peeps array.
+let numberOfPeople = 0;
+for (let i = 0; i < peeps.length; i++) {
+  numberOfPeople += i;
+}
+console.log(numberOfPeople);
 
 // 2. List full names of people in a new array of strings.
+const fullName = peeps.map((person) => {
+  const {
+    name: { first, last },
+  } = person;
+  return `${first} ${last}`;
+});
+
+console.log(fullName);
 
 // 3. Return/save in a variable  “true” if everyone is older than 24 and save in a constant use peeps array Ex: allAre24.
+const adultsOlder = peeps.every((adult) => adult.age > 24);
+console.log(adultsOlder);
 
 // 4. Return/save in a variable “true” if at least one person is younger than 26.
+const minor = peeps.some((child) => child.age < 26);
+console.log(minor);
 
 // 5. Return/save in a variable a new array called “young peeps” of all peeps less than 30.
+const youngPeeps = peeps.filter((person) => person.age < 30);
+console.log(youngPeeps);
 
 // 6. Sort the peeps by age from oldest to youngest in an new array called “sortedPeeps”. use Array.sort()
+// const sortedPeeps = peeps.sort((a, b) => b.age - a.age);
+// console.log(sortedPeeps);
+//modifica array-ul initial
 
 // 7. Create a new array called “firstNamePeeps” with only the first names of the peeps.
+const firstNamePeeps = peeps.map((peep) => {
+  const { name } = peep;
+  return name.first;
+});
+console.log(firstNamePeeps);
 
 // HARDER CHALLENGES (using the comments object and peeps array) //
 
 // 1. List all the comments text  in an array of strings called “commentsTextArray” use array.push() or even harder use array.map()
+let commentsTextArray = [];
+const commentsArray = Object.values(comments);
+commentsArray.forEach((comment) => {
+  commentsTextArray.push(comment.text);
+});
+console.log(commentsTextArray);
+console.log(commentsArray);
 
-// 2. Return all comments with the word “love” in the comment text in a new array called “loveComments” 
+// 2. Return all comments with the word “love” in the comment text in a new array called “loveComments”
+const loveComments = [];
+commentsArray.map((item) => {
+  const { text } = item;
+  // text.includes("love") ? loveComments.push(text) : loveComments;
+  const loveText = text.includes("love");
+  if (loveText) {
+    loveComments.push(text);
+  }
+});
+console.log(loveComments);
 
 // 3. List all the comments in an array called “sortedCommentsArray" from lowest rating to highest rating. Ignore comments without a rating.
+const ratingArray = commentsArray.filter((element) => element.rating);
+const sortedCommentsArray = ratingArray.sort((a, b) => a.rating - b.rating);
+console.log(sortedCommentsArray);
 
 // 4. Return a new object called “commentObj” with the comment id as a key and the comment text as the value.
-
-// 5. Return a new object called “groupedRatings” with the rating as a key and an array of comments text with that rating as the value. 
+const commnetObj = Object.entries(comments);
+console.log(commnetObj);
+// 5. Return a new object called “groupedRatings” with the rating as a key and an array of comments text with that rating as the value.
 // Ignore comments without a rating.
 
 // 6. Return the average rating of all comments.
 
 // 7. Group all comments by the user who made the comment. Return a new object called “groupedPeepComments” with the user’s first and last name as a camelcase string key. The value of each object should be an array of comment objects by the person.
 
-
+// <email>/session_3/exercisess
